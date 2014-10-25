10e885131a6873c47ba3091a4feea668087008ae
aleftik <aleftik@appdynamics.com>
2014-10-24 20:41:43 -0700

    changes to prevent collisions with CONTROLLER

--- ECommerce-Tomcat/startup.sh
+++ ECommerce-Tomcat/startup.sh
@@ -10,7 +10,7 @@ if [ -z "${APPD_PORT}" ]; then
 	export APPD_PORT=8090;
 fi
 
-if [-z "JVM_ROUTE" ]; then
+if [ -z "JVM_ROUTE" ]; then
 	export JVM_ROUTE="route1";
 fi
 
@@ -55,7 +55,7 @@ if [ -n "${ws}" ]; then
 fi
 
 JAVA_OPTS="-Dappdynamics.controller.hostName=${CONTROLLER} -Dappdynamics.controller.port=${APPD_PORT} -Dappdynamics.agent.applicationName=${APP_NAME} -Dappdynamics.agent.tierName=${TIER_NAME} -Dappdynamics.agent.nodeName=${NODE_NAME}"
-//
+
 JAVA_OPTS="${JAVA_OPTS} -DjvmRoute=${JVM_ROUTE} -Xmx512m -XX:MaxPermSize=128m -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager -Dappdynamics.agent.uniqueHostId=cart-machine";
 
 echo $JAVA_OPTS;
