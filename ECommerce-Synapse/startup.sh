7f781cf8c88783541548360452e50a207016b523
aleftik <aleftik@appdynamics.com>
2014-11-16 09:36:42 -0800

    Synapse Changes for 4.0

--- ECommerce-Synapse/startup.sh
+++ ECommerce-Synapse/startup.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
-cd ${SYNAPSE_HOME};
 
-bin/synapse.sh
+if [ -z "${CONTROLLER}" ]; then
+	export CONTROLLER="controller";
+fi
+
+if [ -z "${APPD_PORT}" ]; then
+	export APPD_PORT=8090;
+fi
+
+if [ -z "${APP_NAME}" ]; then
+	export APP_NAME="Fulfillment";
+fi
+
+if [ -z "${NODE_NAME}" ]; then
+	export NODE_NAME="SynapseESB1";
+fi
+		
+if [ -z "${TIER_NAME}" ]; then
+	export TIER_NAME="FulfillmentESB";
+fi
+
+JAVA_OPTS="-Dappdynamics.controller.hostName=${CONTROLLER} -Dappdynamics.controller.port=${APPD_PORT} -Dappdynamics.agent.applicationName=${APP_NAME} -Dappdynamics.agent.tierName=${TIER_NAME} -Dappdynamics.agent.nodeName=${NODE_NAME}";
+
+echo java ${JAVA_OPTS} -jar ${MACHINE_AGENT_HOME}/machineagent.jar
+
+nohup java ${JAVA_OPTS} -jar ${MACHINE_AGENT_HOME}/machineagent.jar  > ${MACHINE_AGENT_HOME}/machine_agent.log 2>&1 &
+
+
+cd ${SYNAPSE_HOME};
+bin/synapse.sh	
