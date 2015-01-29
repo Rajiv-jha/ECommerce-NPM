2df6a48b58a278e63745f7cb9bfa72c61f5b025c
Mark Prichard <mqprichard@gmail.com>
2015-01-28 18:37:43 -0800

    Added startup parameters

---
+++ ECommerce-Load/env.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# 1st param to LoadRunner call
+if [ -z "${NUM_OF_USERS}" ]; then
+        export NUM_OF_USERS="5";
+fi
+
+# 2nd param to LoadRunner call
+if [ -z "${RAMP_TIME}" ]; then
+        export RAMP_TIME="5";
+fi
+
+# 3rd param to LoadRunner call
+if [ -z "${TIME_BETWEEN_RUNS}" ]; then
+        export TIME_BETWEEN_RUNS="5000";
+fi
+
+# 4th param to LoadRunner call
+if [ -z "${TARGET_HOST}" ]; then
+	export TARGET_HOST="lbr";
+fi
+
+# 5th param to LoadRunner call
+if [ -z "${TARGET_PORT}" ]; then
+        export TARGET_PORT="80";
+fi
+
+# 6th param to LoadRunner call
+if [ -z "${WAIT_TIME}" ]; then
+        export WAIT_TIME="1000";
+fi
+
--- ECommerce-Load/startup.sh
+++ ECommerce-Load/startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+source env.sh
+
 nohup Xvfb :10 -ac &
 export DISPLAY=:10;
-$LOAD_GEN_HOME/bin/ECommerce-Load 5  5 5000 lbr 80 1000
+
+$LOAD_GEN_HOME/bin/ECommerce-Load $NUM_OF_USERS $RAMP_TIME $TIME_BETWEEN_RUNS $TARGET_HOST $TARGET_PORT $WAIT_TIME
 
