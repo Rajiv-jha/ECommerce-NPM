13817727c8630de37c855afc484e0856952ba7ef
Mark Prichard <mqprichard@gmail.com>
2015-01-29 16:11:26 -0800

    Reformatted build script

--- build.sh
+++ build.sh
@@ -18,56 +18,57 @@ then
 
 else
 
-if [[ $1 == *--download* ]]
-# Download App Server, Machine and Database Agent from download.appdynamics.com
-# Requires an AppDynamics portal login: prompt user for email/password
-then
-  # Requires an AppDynamics portal login to download AppServer, Machine and DB Agents
-
-  echo "Please Sign In to download agent..."
-  echo "Email ID/UserName: "
-  read USER_NAME
-
-  stty -echo
-  echo "Password: "
-  read PASSWORD
-  stty echo
-
-  if [ "$USER_NAME" != "" ] && [ "$PASSWORD" != "" ];
+  if [[ $1 == *--download* ]]
+  # Download App Server, Machine and Database Agent from download.appdynamics.com
+  # Requires an AppDynamics portal login: prompt user for email/password
   then
-    mkdir -p .agents
+    # Requires an AppDynamics portal login to download AppServer, Machine and DB Agents
 
-    wget --quiet --save-cookies cookies.txt  --post-data "username=$USER_NAME&password=$PASSWORD" --no-check-certificate https://login.appdynamics.com/sso/login/
+    echo "Please Sign In to download agent..."
+    echo "Email ID/UserName: "
+    read USER_NAME
 
-    echo "Downloading AppServerAgent..."
-    wget --quiet --load-cookies cookies.txt https://download.appdynamics.com/onpremise/public/latest/AppServerAgent.zip -O .agents/AppServerAgent.zip
-    if [ $? -ne 0 ]; then
-      rm cookies.txt index.html*
-      exit 
-    fi
-    APP_SERVER_AGENT=".agents/AppServerAgent.zip"
+    stty -echo
+    echo "Password: "
+    read PASSWORD
+    stty echo
 
-    echo "Downloading MachineAgent..."
-    wget --quiet --load-cookies cookies.txt https://download.appdynamics.com/onpremise/public/latest/MachineAgent.zip -O .agents/MachineAgent.zip
-    if [ $? -ne 0 ]; then
-      rm cookies.txt index.html*
-      exit 
-    fi
-    MACHINE_AGENT=".agents/MachineAgent.zip"
+    if [ "$USER_NAME" != "" ] && [ "$PASSWORD" != "" ];
+    then
+      mkdir -p .agents
 
-    echo "Downloading DBAgent..."
-    wget --quiet --load-cookies cookies.txt https://download.appdynamics.com/onpremise/public/latest/dbagent.zip -O .agents/dbagent.zip
-    if [ $? -ne 0 ]; then
-      rm cookies.txt index.html*
-      exit 
-    fi
-    DB_AGENT=".agents/dbagent.zip"
+      wget --quiet --save-cookies cookies.txt  --post-data "username=$USER_NAME&password=$PASSWORD" --no-check-certificate https://login.appdynamics.com/sso/login/
 
-    else
-      echo "Username or Password missing"
-    fi
-else
-# Allow user to specify locations of App Server, Machine and Database Agents
+      echo "Downloading AppServerAgent..."
+      wget --quiet --load-cookies cookies.txt https://download.appdynamics.com/onpremise/public/latest/AppServerAgent.zip -O .agents/AppServerAgent.zip
+      if [ $? -ne 0 ]; then
+        rm cookies.txt index.html*
+        exit 
+      fi
+      APP_SERVER_AGENT=".agents/AppServerAgent.zip"
+
+      echo "Downloading MachineAgent..."
+      wget --quiet --load-cookies cookies.txt https://download.appdynamics.com/onpremise/public/latest/MachineAgent.zip -O .agents/MachineAgent.zip
+      if [ $? -ne 0 ]; then
+        rm cookies.txt index.html*
+        exit 
+      fi
+      MACHINE_AGENT=".agents/MachineAgent.zip"
+
+      echo "Downloading DBAgent..."
+      wget --quiet --load-cookies cookies.txt https://download.appdynamics.com/onpremise/public/latest/dbagent.zip -O .agents/dbagent.zip
+      if [ $? -ne 0 ]; then
+        rm cookies.txt index.html*
+        exit 
+      fi
+      DB_AGENT=".agents/dbagent.zip"
+
+      else
+        echo "Username or Password missing"
+      fi
+
+  else
+    # Allow user to specify locations of App Server, Machine and Database Agents
     while getopts "a:m:d:" opt; do
       case $opt in
         a)
@@ -98,8 +99,10 @@ else
           echo "Invalid option: -$OPTARG"
           ;;
       esac
-  done
-fi
+    done
+
+  fi
+
 fi
 
 # Pull Java base image from appdynamics docker repo
