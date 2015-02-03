b591239675225dc876ba68ffa59942b64b320376
Mark Prichard <mqprichard@gmail.com>
2015-02-02 22:28:44 -0800

    Refactored build.sh: updating MachineAgetn.zip in-place

--- build.sh
+++ build.sh
@@ -1,5 +1,76 @@
 #! /bin/bash
 
+cleanUp() {
+  rm -f cookies.txt
+  rm -f index.html*
+  rm -rf .agents
+  (cd ECommerce-Tomcat && rm -f AppServerAgent.zip MachineAgent.zip)
+  (cd ECommerce-Tomcat && rm -rf monitors)
+  (cd ECommerce-Synapse && rm -f AppServerAgent.zip MachineAgent.zip)
+  (cd ECommerce-DBAgent && rm -f dbagent.zip)
+
+  # Remove dangling images left-over from build
+  if [[ `docker images -q --filter "dangling=true"` ]]
+  then
+    echo
+    echo "Deleting intermediate containers..."
+    docker images -q --filter "dangling=true" | xargs docker rmi;
+  fi
+}
+
+promptForAgents() {
+  read -e -p "Enter path to App Server Agent: " APP_SERVER_AGENT
+  read -e -p "Enter path to Machine Agent: " MACHINE_AGENT
+  read -e -p "Enter path to DB Agent: " DB_AGENT
+}
+
+promptForAnalytics() {
+  read -e -p "Enter Analytics Account Name: " ANALYTICS_ACCOUNT_NAME
+  read -e -p "Enter Analytics Account Key: " ANALYTICS_ACCOUNT_KEY
+}
+
+promptForPortal() {
+  echo "Please Sign In to download agent..."
+  echo "Email ID/UserName: "
+  read USER_NAME
+
+  stty -echo
+  echo "Password: "
+  read PASSWORD
+  stty echo
+}
+
+downloadAgents() {
+  mkdir -p .agents
+
+  wget --quiet --save-cookies cookies.txt  --post-data "username=$USER_NAME&password=$PASSWORD" --no-check-certificate https://login.appdynamics.com/sso/login/
+
+  echo "Downloading AppServerAgent..."
+  wget --quiet --load-cookies cookies.txt https://download.appdynamics.com/onpremise/public/latest/AppServerAgent.zip -O .agents/AppServerAgent.zip
+  if [ $? -ne 0 ]; then
+     cleanUp
+     exit
+  fi
+  APP_SERVER_AGENT=".agents/AppServerAgent.zip"
+
+  echo "Downloading MachineAgent..."
+  wget --quiet --load-cookies cookies.txt https://download.appdynamics.com/onpremise/public/latest/MachineAgent.zip -O .agents/MachineAgent.zip
+  if [ $? -ne 0 ]; then
+     cleanUp
+     exit
+  fi
+  MACHINE_AGENT=".agents/MachineAgent.zip"
+
+  echo "Downloading DBAgent..."
+  wget --quiet --load-cookies cookies.txt https://download.appdynamics.com/onpremise/public/latest/dbagent.zip -O .agents/dbagent.zip
+  if [ $? -ne 0 ]; then
+     cleanUp
+     exit
+  fi
+  DB_AGENT=".agents/dbagent.zip"
+}
+
+
 # Usage information
 if [[ $1 == *--help* ]]
 then
@@ -12,71 +83,37 @@ fi
 if  [ $# -eq 0 ]
 # Prompt for location of App Server, Machine and Database Agents
 then   
-  read -e -p "Enter path to App Server Agent: " APP_SERVER_AGENT
-  read -e -p "Enter path to Machine Agent: " MACHINE_AGENT
-  read -e -p "Enter path to DB Agent: " DB_AGENT
+  promptForAgents
+  promptForAnalytics
 
 else
-
   if [[ $1 == *--download* ]]
   # Download App Server, Machine and Database Agent from download.appdynamics.com
-  # Requires an AppDynamics portal login: prompt user for email/password
   then
-    # Requires an AppDynamics portal login to download AppServer, Machine and DB Agents
-
-    echo "Please Sign In to download agent..."
-    echo "Email ID/UserName: "
-    read USER_NAME
-
-    stty -echo
-    echo "Password: "
-    read PASSWORD
-    stty echo
+    # Requires an AppDynamics portal login
+    promptForPortal
 
     if [ "$USER_NAME" != "" ] && [ "$PASSWORD" != "" ];
     then
-      mkdir -p .agents
+      downloadAgents
+    else
+      echo "Username or Password missing"
+      exit
+    fi
 
-      wget --quiet --save-cookies cookies.txt  --post-data "username=$USER_NAME&password=$PASSWORD" --no-check-certificate https://login.appdynamics.com/sso/login/
-
-      echo "Downloading AppServerAgent..."
-      wget --quiet --load-cookies cookies.txt https://download.appdynamics.com/onpremise/public/latest/AppServerAgent.zip -O .agents/AppServerAgent.zip
-      if [ $? -ne 0 ]; then
-        rm cookies.txt index.html*
-        exit 
-      fi
-      APP_SERVER_AGENT=".agents/AppServerAgent.zip"
-
-      echo "Downloading MachineAgent..."
-      wget --quiet --load-cookies cookies.txt https://download.appdynamics.com/onpremise/public/latest/MachineAgent.zip -O .agents/MachineAgent.zip
-      if [ $? -ne 0 ]; then
-        rm cookies.txt index.html*
-        exit 
-      fi
-      MACHINE_AGENT=".agents/MachineAgent.zip"
-
-      echo "Downloading DBAgent..."
-      wget --quiet --load-cookies cookies.txt https://download.appdynamics.com/onpremise/public/latest/dbagent.zip -O .agents/dbagent.zip
-      if [ $? -ne 0 ]; then
-        rm cookies.txt index.html*
-        exit 
-      fi
-      DB_AGENT=".agents/dbagent.zip"
-
-      else
-        echo "Username or Password missing"
-      fi
+    # Prompt for Analytics Account Name/Key
+    promptForAnalytics 
 
   else
     # Allow user to specify locations of App Server, Machine and Database Agents
-    while getopts "a:m:d:" opt; do
+    while getopts "a:m:d:n:k:" opt; do
       case $opt in
         a)
           APP_SERVER_AGENT=$OPTARG
           if [ ! -e ${APP_SERVER_AGENT} ]
           then
             echo "Not found: ${APP_SERVER_AGENT}"
-          exit
+            exit
           fi
           ;;
         m)
@@ -92,15 +129,21 @@ else
           if [ ! -e ${DB_AGENT} ]
           then
             echo "Not found: ${DB_AGENT}"
-          exit
+            exit
           fi
+          ;;
+        n)
+          ANALYTICS_ACCOUNT_NAME=$OPTARG
+          ;;
+        k)
+          ANALYTICS_ACCOUNT_KEY=$OPTARG
           ;;
         \?)
           echo "Invalid option: -$OPTARG"
           ;;
       esac
     done
-
+  
   fi
 
 fi
@@ -108,15 +151,32 @@ fi
 # Pull Java base image from appdynamics docker repo
 docker pull appdynamics/ecommerce-java:oracle-java7
 
-# Build Tomcat containers using downloaded AppServer and Machine Agents
-# Use docker build with --no-cache option to force latest git repo clone
+# Copy Agent zips to build dirs
 cp ${APP_SERVER_AGENT} ECommerce-Tomcat/AppServerAgent.zip
 cp ${MACHINE_AGENT} ECommerce-Tomcat/MachineAgent.zip
+cp ${APP_SERVER_AGENT} ECommerce-Synapse/AppServerAgent.zip
+cp ${MACHINE_AGENT} ECommerce-Synapse/MachineAgent.zip
+
+if [ "$ANALYTICS_ACCOUNT_NAME" != "" ] && [ "$ANALYTICS_ACCOUNT_KEY" != "" ];
+then
+  # Update Machine Agent install with Analytics account/key info
+  (cd ECommerce-Tomcat && unzip MachineAgent.zip monitors/analytics-agent/conf/analytics-agent.properties)
+  (cd ECommerce-Tomcat && sed -i .bak "s/analytics-customer1/$ANALYTICS_ACCOUNT_NAME/g" monitors/analytics-agent/conf/analytics-agent.properties)
+  (cd ECommerce-Tomcat && sed -i .bak "s/your-account-access-key/$ANALYTICS_ACCOUNT_KEY/g" monitors/analytics-agent/conf/analytics-agent.properties)
+  (cd ECommerce-Tomcat && zip MachineAgent.zip monitors/analytics-agent/conf/analytics-agent.properties)
+
+  # Enable Analytics 
+  (cd ECommerce-Tomcat && unzip MachineAgent.zip monitors/analytics-agent/monitor.xml)
+  (cd ECommerce-Tomcat && sed -i .bak "s/false/true/g" monitors/analytics-agent/monitor.xml)
+  (cd ECommerce-Tomcat && zip MachineAgent.zip monitors/analytics-agent/monitor.xml)
+fi
+
+# Build Tomcat containers using downloaded AppServer and Machine Agents
+# Use docker build with --no-cache option to force latest git repo clone
 (cd ECommerce-Tomcat && docker build --no-cache -t appdynamics/ecommerce-tomcat .)
 
 # Build Synapse container using downloaded AppServer and Machine Agents
-cp ${APP_SERVER_AGENT} ECommerce-Synapse/AppServerAgent.zip
-cp ${MACHINE_AGENT} ECommerce-Synapse/MachineAgent.zip
+cp ECommerce-Tomcat/MachineAgent.zip ECommerce-Synapse/MachineAgent.zip
 (cd ECommerce-Synapse && docker build -t appdynamics/ecommerce-synapse .)
 
 # Build DBAgent container using downloaded DBAgent
@@ -139,19 +199,5 @@ echo "    appdynamics/ecommerce-lbr"
 echo "    appdynamics/ecommerce-load"
 echo "    appdynamics/ecommerce-oracle"
 
-# Cleanup
-rm -f cookies.txt
-rm -f index.html*
-rm -rf .agents
-(cd ECommerce-Tomcat && rm -f AppServerAgent.zip MachineAgent.zip)
-(cd ECommerce-Synapse && rm -f AppServerAgent.zip MachineAgent.zip)
-(cd ECommerce-DBAgent && rm -f dbagent.zip)
-
-# Remove dangling images left-over from build
-if [[ `docker images -q --filter "dangling=true"` ]] 
-then
-	echo
-	echo "Deleting intermediate containers..."
-	docker images -q --filter "dangling=true" | xargs docker rmi;
-fi
+cleanUp
 
