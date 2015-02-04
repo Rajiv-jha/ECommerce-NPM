1b728efc7d24a73c649ab3d2bfb406add1ab8119
Mark Prichard <mqprichard@gmail.com>
2015-02-03 17:13:01 -0800

    Updated run script

--- run.sh
+++ run.sh
@@ -1,20 +1,32 @@
-# MySQL database and DB Agent
+#!/bin/sh
+
+APP_NAME=$1
+CONTR_HOST=controller
+CONTR_PORT=8090
+if [ -z "$1" ]; then
+        export APP_NAME="ECommerce";
+else
+        export APP_NAME=$1;
+fi
+
+docker run --name oracle-db -d -p 1521:1521 appdynamics/ecommerce-oracle
 docker run --name db -e MYSQL_ROOT_PASSWORD=singcontroller -p 3306:3306 -d mysql
-docker run --rm -it --name dbagent --link controller:controller --link db:db appdynamics/ecommerce-dbagent
-docker run --rm -it --name oracle-db  appdynamics/ecommerce-oracle
+docker run --name jms -d appdynamics/ecommerce-activemq
+sleep 30
 
-# ActiveMQ service
-docker run --rm -it --name jms appdynamics/ecommerce-activemq
+docker run --name ws -e create_schema=true -e ws=true -e NODE_NAME=${APP_NAME}_WS_NODE -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e APP_NAME=$APP_NAME --link db:db --link jms:jms --link oracle-db:oracle-db --link controller:controller -d appdynamics/ecommerce-tomcat
+docker run --name web -e NODE_NAME=${APP_NAME}_WEB1_NODE -e JVM_ROUTE=route1 -e web=true -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e APP_NAME=$APP_NAME --link db:db --link ws:ws --link jms:jms --link controller:controller -d appdynamics/ecommerce-tomcat
+sleep 30
 
-# Tomcat-based services
-docker run --rm -it --name ws -e ws=true --link controller:controller --link db:db --link jms:jms appdynamics/ecommerce-tomcat
-docker run --rm -it --name fulfillment -e web=true -e NODE_NAME=fulfillment -e APP_NAME=Fulfillment -e TIER_NAME=Fulfillment-Processor --link controller:controller --link db:db  --link oracle-db:oracle-db --link ws:ws --link jms:jms   appdynamics/ecommerce-tomcat
-docker run --rm -it --name web -e web=true -e JVM_ROUTE=route1 --link oracle-db:oracle-db --link controller:controller --link db:db --link ws:ws --link jms:jms appdynamics/ecommerce-tomcat
-docker run --rm -it --name web1 -e web=true -e JVM_ROUTE=route2 --link oracle-db:oracle-db --link controller:controller --link db:db --link ws:ws --link jms:jms appdynamics/ecommerce-tomcat
-docker run --rm -it --name msg -e jms=true --link controller:controller --link db:db --link jms:jms --link fulfillment:fulfillment appdynamics/ecommerce-tomcat
+docker run --name fulfillment -e web=true -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e NODE_NAME=Fulfillment -e APP_NAME=Fulfillment -e TIER_NAME=Fulfillment-Processor --link db:db --link ws:ws --link jms:jms --link oracle-db:oracle-db --link controller:controller -d appdynamics/ecommerce-tomcat
+sleep 30
 
-# Apache httpd LBR service
-docker run --rm -it --name lbr --link web:web --link web1:web1 -p 80:80  appdynamics/ecommerce-lbr
+docker run --name web1 -e web=true -e NODE_NAME=${APP_NAME}_WEB2_NODE -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e JVM_ROUTE=route2 -e APP_NAME=$APP_NAME --link db:db --link ws:ws --link jms:jms --link controller:controller -d appdynamics/ecommerce-tomcat
+sleep 30
 
-# Selenium load generator
-docker run --rm -it --name load-gen --link lbr:lbr appdynamics/ecommerce-load
+docker run --name=lbr --link web:web --link web1:web1 -p 80:80 -d appdynamics/ecommerce-lbr
+docker run --name msg -e jms=true -e NODE_NAME=${APP_NAME}_JMS_NODE -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e APP_NAME=$APP_NAME --link db:db --link jms:jms --link oracle-db:oracle-db --link fulfillment:fulfillment --link controller:controller -d appdynamics/ecommerce-tomcat
+sleep 30
+
+docker run --name=load-gen --link lbr:lbr -d appdynamics/ecommerce-load
+docker run --name dbagent -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} --link db:db --link oracle-db:oracle-db --link controller:controller -d appdynamics/ecommerce-dbagent
