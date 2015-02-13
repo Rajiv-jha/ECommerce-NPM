d311bc08f850a348d2b19e7dcbb88c78c94eccdd
Mark Prichard <mqprichard@gmail.com>
2015-02-13 08:01:43 -0800

    Using appdynamics/ecommerce-java:java-7 as base image

--- ECommerce-Apache/start-machine-agent.sh
+++ ECommerce-Apache/start-machine-agent.sh
@@ -1,4 +1,3 @@
 echo "Starting Machine Agent..."
 source /opt/appdynamics/env.sh
-echo java ${AGENT_OPTS} -jar ${MACHINE_AGENT_HOME}/machineagent.jar
-nohup java ${AGENT_OPTS} -jar ${MACHINE_AGENT_HOME}/machineagent.jar  > ${MACHINE_AGENT_HOME}/machine_agent.log 2>&1 &
+nohup java ${AGENT_OPTS} -jar ${MACHINE_AGENT_HOME}/machineagent.jar  > ${MACHINE_AGENT_HOME}/machine_agent.log &
