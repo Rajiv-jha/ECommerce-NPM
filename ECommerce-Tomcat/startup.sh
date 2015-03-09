ba8317956e0dea4078a480bc24cfcbe74a1729e9
Mark Prichard <mqprichard@gmail.com>
2015-03-09 11:26:39 -0700

    Refactored agent startup scripts

--- ECommerce-Tomcat/startup.sh
+++ ECommerce-Tomcat/startup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 CWD=${PWD}
 
 source /env.sh
@@ -20,12 +20,6 @@ if [ -n "${ws}" ]; then
 fi
 
 source /start-machine-agent.sh
-
-APP_AGENT_JAVA_OPTS="${MACHINE_AGENT_JAVA_OPTS} -DjvmRoute=${JVM_ROUTE} -Xmx512m -XX:MaxPermSize=128m -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager";
-echo "Starting Tomcat with App Server Agent..."
-echo APP_AGENT_JAVA_OPTS: $APP_AGENT_JAVA_OPTS;
-echo JMX_OPTS: ${JMX_OPTS}
-cd ${CATALINA_HOME}/bin;
-java -javaagent:${CATALINA_HOME}/appagent/javaagent.jar ${APP_AGENT_JAVA_OPTS} ${JMX_OPTS} -cp ${CATALINA_HOME}/bin/bootstrap.jar:${CATALINA_HOME}/bin/tomcat-juli.jar org.apache.catalina.startup.Bootstrap
+source /start-appserver-agent.sh
 
 cd ${CWD}
