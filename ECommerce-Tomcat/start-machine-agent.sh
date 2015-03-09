ed50c1404389a225a46c18911a0dbf4d800c0fa6
Mark Prichard <mqprichard@gmail.com>
2015-03-09 10:31:11 -0700

    Cleaned up agent start scripts

--- ECommerce-Tomcat/start-machine-agent.sh
+++ ECommerce-Tomcat/start-machine-agent.sh
@@ -1,4 +1,10 @@
-echo "Starting Machine Agent..."
+#!/bin/sh
+
 source /env.sh
-echo java ${AGENT_OPTS} -jar ${MACHINE_AGENT_HOME}/machineagent.jar
-nohup java ${AGENT_OPTS} -jar ${MACHINE_AGENT_HOME}/machineagent.jar  > ${MACHINE_AGENT_HOME}/machine_agent.log 2>&1 &
+
+MACHINE_AGENT_JAVA_OPTS="-Dappdynamics.controller.hostName=${CONTROLLER} -Dappdynamics.controller.port=${APPD_PORT} -Dappdynamics.agent.applicationName=${APP_NAME} -Dappdynamics.agent.tierName=${TIER_NAME} -Dappdynamics.agent.nodeName=${NODE_NAME}";
+JMX_OPTS="-Dcom.sun.management.jmxremote.port=8888  -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+echo "Starting Machine Agent..."
+echo MACHINE_AGENT_JAVA_OPTS: ${MACHINE_AGENT_JAVA_OPTS}
+echo JMX_OPTS: ${JMX_OPTS}
+nohup java ${MACHINE_AGENT_JAVA_OPTS} -jar ${MACHINE_AGENT_HOME}/machineagent.jar  > ${MACHINE_AGENT_HOME}/machine_agent.log 2>&1 &
--- ECommerce-Tomcat/startup.sh
+++ ECommerce-Tomcat/startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 CWD=${PWD}
 
-
 source /env.sh
 
 if [ "${create_schema}" == "true" ]; then
@@ -20,21 +19,18 @@ if [ -n "${ws}" ]; then
         cp /ECommerce-Java/ECommerce-WS/build/libs/cart.war /tomcat/webapps;
 fi
 
-JAVA_OPTS="-Dappdynamics.controller.hostName=${CONTROLLER} -Dappdynamics.controller.port=${APPD_PORT} -Dappdynamics.agent.applicationName=${APP_NAME} -Dappdynamics.agent.tierName=${TIER_NAME} -Dappdynamics.agent.nodeName=${NODE_NAME}";
+MACHINE_AGENT_JAVA_OPTS="-Dappdynamics.controller.hostName=${CONTROLLER} -Dappdynamics.controller.port=${APPD_PORT} -Dappdynamics.agent.applicationName=${APP_NAME} -Dappdynamics.agent.tierName=${TIER_NAME} -Dappdynamics.agent.nodeName=${NODE_NAME}";
 JMX_OPTS="-Dcom.sun.management.jmxremote.port=8888  -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 echo "Starting Machine Agent..."
+echo MACHINE_AGENT_JAVA_OPTS: ${MACHINE_AGENT_JAVA_OPTS}
+echo JMX_OPTS: ${JMX_OPTS}
+nohup java ${MACHINE_AGENT_JAVA_OPTS} -jar ${MACHINE_AGENT_HOME}/machineagent.jar  > ${MACHINE_AGENT_HOME}/machine_agent.log 2>&1 &
 
-echo java ${JAVA_OPTS} -jar ${MACHINE_AGENT_HOME}/machineagent.jar
-
-
-nohup java ${JAVA_OPTS} -jar ${MACHINE_AGENT_HOME}/machineagent.jar  > ${MACHINE_AGENT_HOME}/machine_agent.log 2>&1 &
-
-JAVA_OPTS="${JAVA_OPTS} -DjvmRoute=${JVM_ROUTE} -Xmx512m -XX:MaxPermSize=128m -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager";
-
-echo $JAVA_OPTS;
-
+APP_AGENT_JAVA_OPTS="${MACHINE_AGENT_JAVA_OPTS} -DjvmRoute=${JVM_ROUTE} -Xmx512m -XX:MaxPermSize=128m -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager";
+echo "Starting Tomcat with App Server Agent..."
+echo APP_AGENT_JAVA_OPTS: $APP_AGENT_JAVA_OPTS;
+echo JMX_OPTS: ${JMX_OPTS}
 cd ${CATALINA_HOME}/bin;
-
-java -javaagent:${CATALINA_HOME}/appagent/javaagent.jar ${JAVA_OPTS} ${JMX_OPTS} -cp ${CATALINA_HOME}/bin/bootstrap.jar:${CATALINA_HOME}/bin/tomcat-juli.jar org.apache.catalina.startup.Bootstrap
+java -javaagent:${CATALINA_HOME}/appagent/javaagent.jar ${APP_AGENT_JAVA_OPTS} ${JMX_OPTS} -cp ${CATALINA_HOME}/bin/bootstrap.jar:${CATALINA_HOME}/bin/tomcat-juli.jar org.apache.catalina.startup.Bootstrap
 
 cd ${CWD}
