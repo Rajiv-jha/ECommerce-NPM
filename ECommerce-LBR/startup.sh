f1197a13d9c4b0f37523701877f8dfce46b15252
aleftik <aleftik@appdynamics.com>
2015-03-27 15:16:15 -0700

    Changes to try and get around apache issues

--- ECommerce-LBR/startup.sh
+++ ECommerce-LBR/startup.sh
@@ -5,7 +5,8 @@ source /opt/appdynamics/env.sh
 HTTPD24=/opt/rh/httpd24/root/etc/httpd
 
 # Configure Apache for AppDynamics env vars
-#sed -i '1iSetEnv APPD_SDK_ENV_LOG_CONFIG_PATH ${NATIVE_HOME}/conf/appdynamics_sdk_log4cxx.xml' $HTTPD24/conf/httpd.conf
+sed -i '1iSetEnv APPD_SDK_ENV_LOG_CONFIG_PATH ${NATIVE_HOME}/conf/appdynamics_sdk_log4cxx.xml' $HTTPD24/conf/httpd.conf
+
 sed -i "s/<your_controller>/${CONTROLLER}/g" $HTTPD24/conf.d/appd.conf
 sed -i "s/<your_controller_port>/${APPD_PORT}/g" $HTTPD24/conf.d/appd.conf
 sed -i "s/<your_app_name>/${APP_NAME}/g" $HTTPD24/conf.d/appd.conf
@@ -18,8 +19,9 @@ echo "ServerName `hostname`:80" >> $HTTPD24/conf/httpd.conf
 # Start Apache AppDynamics Proxy Agent
 # Start AppDynamics Proxy Agent
 echo "Starting Proxy Agent"
-nohup /opt/appdynamics/appdynamics-sdk-native/runSDKProxy.sh &
+su  apache -c "nohup /opt/appdynamics/appdynamics-sdk-native/runSDKProxy.sh &"
 
+echo 'export APPD_SDK_ENV_LOG_CONFIG_PATH=/opt/appdynamics/appdynamics-sdk-native/conf/appdynamics_sdk_log4cxx.xml' >>  /home/apache/.bash_profile
 /etc/init.d/httpd24-httpd stop
 /etc/init.d/httpd24-httpd start
 
