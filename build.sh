36fd8f3d1c170a322674be72d9c8e00573653992
Mark Prichard <mqprichard@gmail.com>
2015-04-10 10:57:12 -0700

    Separate download for Oracle Java JDK

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 cleanUp() {
+  (cd ECommerce-Java && rm -f jdk-linux-x64.rpm)
   (cd ECommerce-Tomcat && rm -f AppServerAgent.zip MachineAgent.zip)
   (cd ECommerce-Tomcat && rm -rf monitors ECommerce-Java)
   (cd ECommerce-Synapse && rm -f AppServerAgent.zip MachineAgent.zip)
@@ -76,15 +77,22 @@ else
   done
 fi
 
-# Pull Java base image from appdynamics docker repo
-docker pull appdynamics/ecommerce-java:oracle-java7
+# Pull Java base image from (private) appdynamics docker repo
+# docker pull appdynamics/ecommerce-java:oracle-java7
+
+# Download Oracle JDK7 and build ecommerce-java base image
+echo "Building ECommerce-Java base image..."
+(cd ECommerce-Java; curl -j -k -L -H "Cookie:oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u71-b13/jdk-7u71-linux-x64.rpm -o jdk-linux-x64.rpm)
+(cd ECommerce-Java; docker build -t appdynamics/ecommerce-java .)
 
 # Copy Agent zips to build dirs
+echo "Adding AppDynamics Agents..."
 cp ${APP_SERVER_AGENT} ECommerce-Tomcat/AppServerAgent.zip
 cp ${APP_SERVER_AGENT} ECommerce-Synapse/AppServerAgent.zip
 cp ${MACHINE_AGENT} ECommerce-Tomcat/MachineAgent.zip
 
 # Enable Analytics 
+echo "Configuring Events Service..."
 (cd ECommerce-Tomcat && unzip MachineAgent.zip monitors/analytics-agent/monitor.xml)
 if [ `uname` == "Darwin" ]; then
   (cd ECommerce-Tomcat && sed -i .bak "s/false/true/g" monitors/analytics-agent/monitor.xml)
@@ -94,22 +102,27 @@ fi
 (cd ECommerce-Tomcat && zip MachineAgent.zip monitors/analytics-agent/monitor.xml)
 
 # Build Tomcat containers using downloaded AppServer and Machine Agents
+echo "Building ECommerce-Tomcat..."
 (cd ECommerce-Tomcat && git clone https://github.com/Appdynamics/ECommerce-Java.git)
 (cd ECommerce-Tomcat && docker build -t appdynamics/ecommerce-tomcat .)
 
 # Build Synapse container using downloaded AppServer and Machine Agents
+echo "Building ECommerce-Synapse..."
 cp ECommerce-Tomcat/MachineAgent.zip ECommerce-Synapse/MachineAgent.zip
 (cd ECommerce-Synapse && docker build -t appdynamics/ecommerce-synapse .)
 
 # Build DBAgent container using downloaded DBAgent
+echo "Building ECommerce-DBAgent..."
 cp ${DB_AGENT} ECommerce-DBAgent/dbagent.zip
 (cd ECommerce-DBAgent && docker build -t appdynamics/ecommerce-dbagent .)
 
 # Build LoadGen container
+echo "Building ECommerce-Load..."
 (cd ECommerce-Load && git clone https://github.com/Appdynamics/ECommerce-Load.git)
 (cd ECommerce-Load && docker build -t appdynamics/ecommerce-load .)
 
-# Pull ActiveMQ, LBR and LoadGen containers from appdynamics public docker repo
+# Pull ActiveMQ, LBR and Oracle containers from (private) appdynamics docker repo
+echo "Pulling ActiveMQ, LBR and Oracle database containers..."
 docker pull appdynamics/ecommerce-activemq
 docker pull appdynamics/ecommerce-lbr
 docker pull appdynamics/ecommerce-oracle
