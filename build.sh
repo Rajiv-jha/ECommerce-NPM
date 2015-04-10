c2b510b939fc018f3f1d813ab57f196210dc8b53
Mark Prichard <mqprichard@gmail.com>
2015-04-10 11:35:33 -0700

    Added instructions to sample build/run scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
+# This script is provided for illustration purposes only.
+#
+# To build the ECommerce demo application, you will need to download the following components:
+# 1. An appropriate version of the Oracle Java 7 JDK
+#    (http://www.oracle.com/technetwork/java/javase/downloads/index.html)
+# 2. Correct versions for the AppDynamics AppServer Agent, Machine Agent and Database Monitoring Agent for your Controller installation
+#    (https://download.appdynamics.com)
+#
+# To run the ECommerce demo application, you will also need to:
+# 1. Build and run the ECommerce-Oracle docker container
+#    The Dockerfile is available here (https://github.com/Appdynamics/ECommerce-Docker/tree/master/ECommerce-Oracle)
+#    The container requires you to downlaod an appropriate version of the Oracle Database Express Edition 11g Release 2
+#    (http://www.oracle.com/technetwork/database/database-technologies/express-edition/downloads/index.html)
+# 2. Download and run the official Docker mysql container
+#    (https://registry.hub.docker.com/_/mysql/)
+ 
 #! /bin/bash
 
 cleanUp() {
