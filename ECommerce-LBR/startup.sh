822abdc96ae4e5e715d06aab1454ce16ff784b09
aleftik <aleftik@appdynamics.com>
2015-04-13 17:56:36 -0700

    Fixes for naming

--- ECommerce-LBR/startup.sh
+++ ECommerce-LBR/startup.sh
@@ -23,7 +23,9 @@ source /opt/appdynamics-sdk-native/env.sh
 AVAIL_ZONE=`curl http://169.254.169.254//latest/meta-data/placement/availability-zone`
 CLOUD_NAME=`curl http://169.254.169.254//latest/meta-data/public-hostname`
 
-sed -e "s/CONTROLLERHOST/${CONTROLLER}/g;s/CONTROLLERPORT/${APPD_PORT}/g;s/APP/${APP_NAME}/g;s/TIER/${TIER_NAME}/g;s/NODE/${NODE_NAME}/g;s/FOO/${AVAIL_ZONE}/g;s/BAR/${CLOUD_NAME}/g" /controller-info.xml > $MACHINE_AGENT_HOME/conf/controller-info.xml;service appdynamics-machine-agent start
+sed -e "s/CONTROLLERHOST/${CONTROLLER}/g;s/CONTROLLERPORT/${APPD_PORT}/g;s/APP/${APP_NAME}/g;s/TIER/${TIER_NAME}/g;s/NODE/${NODE_NAME}/g;s/FOO/${AVAIL_ZONE}/g;s/BAR/${CLOUD_NAME}/g;s/BAZ/${HOSTNAME}" /controller-info.xml > $MACHINE_AGENT_HOME/conf/controller-info.xml;
+
+service appdynamics-machine-agent start
 
 # Set Apache ServerName usign container's hostname
 echo "ServerName `hostname`:80" >> $HTTPD24/conf/httpd.conf
--- ECommerce-Tomcat/start-machine-agent.sh
+++ ECommerce-Tomcat/start-machine-agent.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 source /env.sh
+source /env.sh
+AVAIL_ZONE=`curl http://169.254.169.254//latest/meta-data/placement/availability-zone`
 
-sed -e "s/CONTROLLERHOST/${CONTROLLER}/g;s/CONTROLLERPORT/${APPD_PORT}/g;s/APP/${APP_NAME}/g;s/TIER/${TIER_NAME}/g;s/NODE/${NODE_NAME}/g" /controller-info.xml > $MACHINE_AGENT_HOME/conf/controller-info.xml;
+CLOUD_NAME=`curl http://169.254.169.254//latest/meta-data/public-hostname`
+
+sed -e "s/CONTROLLERHOST/${CONTROLLER}/g;s/CONTROLLERPORT/${APPD_PORT}/g;s/APP/${APP_NAME}/g;s/TIER/${TIER_NAME}/g;s/NODE/${NODE_NAME}/g;s/FOO/${AVAIL_ZONE}/g;s/BAR/${CLOUD_NAME}/g;s/BAZ/${HOSTNAME}" /controller-info.xml > $MACHINE_AGENT_HOME/conf/controller-info.xml
+sed -e "s/false/true/g" /monitor.xml > $MACHINE_AGENT_HOME/monitors/analytics-agent/monitor.xml;
 service appdynamics-machine-agent start
