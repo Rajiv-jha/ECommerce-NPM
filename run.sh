0045ba201c1076bcde6528a3c60be93290e36c6d
Mark Prichard <mqprichard@gmail.com>
2015-05-06 15:20:29 -0700

    Added EVENT_ENDPOINT and -h flags to docker run commands

--- run.sh
+++ run.sh
@@ -49,21 +49,21 @@ docker run --name db -e MYSQL_ROOT_PASSWORD=singcontroller -p 3306:3306 -d mysql
 docker run --name jms -d appdynamics/ecommerce-activemq:$VERSION
 sleep 30
 
-docker run --name ws -e create_schema=true -e ws=true -e NODE_NAME=${APP_NAME}_WS_NODE -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e APP_NAME=$APP_NAME --link db:db --link jms:jms --link oracle-db:oracle-db --link controller:controller -d appdynamics/ecommerce-tomcat:$VERSION
-docker run --name web -e NODE_NAME=${APP_NAME}_WEB1_NODE -e JVM_ROUTE=route1 -e web=true -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e APP_NAME=$APP_NAME --link db:db --link ws:ws --link jms:jms --link controller:controller -d appdynamics/ecommerce-tomcat:$VERSION
+docker run --name ws -h ${APP_NAME}-ws -e create_schema=true -e ws=true -e EVENT_ENDPOINT=${EVENT_ENDPOINT} -e NODE_NAME=${APP_NAME}_WS_NODE -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e APP_NAME=$APP_NAME --link db:db --link jms:jms --link oracle-db:oracle-db --link controller:controller -d appdynamics/ecommerce-tomcat:$VERSION
+docker run --name web -h ${APP_NAME}-web i-e EVENT_ENDPOINT=${EVENT_ENDPOINT} -e NODE_NAME=${APP_NAME}_WEB1_NODE -e JVM_ROUTE=route1 -e web=true -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e APP_NAME=$APP_NAME --link db:db --link ws:ws --link jms:jms --link controller:controller -d appdynamics/ecommerce-tomcat:$VERSION
 sleep 30
 
-docker run --name fulfillment -e web=true -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e NODE_NAME=Fulfillment -e APP_NAME=Fulfillment -e TIER_NAME=Fulfillment-Processor --link db:db --link ws:ws --link jms:jms --link oracle-db:oracle-db --link controller:controller -d appdynamics/ecommerce-tomcat:$VERSION
+docker run --name fulfillment -h ${APP_NAME}-fulfillment -e EVENT_ENDPOINT=${EVENT_ENDPOINT} -e web=true -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e NODE_NAME=Fulfillment -e APP_NAME=Fulfillment -e TIER_NAME=Fulfillment-Processor --link db:db --link ws:ws --link jms:jms --link oracle-db:oracle-db --link controller:controller -d appdynamics/ecommerce-tomcat:$VERSION
 sleep 30
 
 docker run --name fulfillment-client -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e NODE_NAME=FulfillmentClient1 -e APP_NAME=Fulfillment -e TIER_NAME=Fulfillment-Client --link controller:controller -d appdynamics/ecommerce-fullfilment-client:$VERSION
 sleep 30
 
-docker run --name web1 -e web=true -e NODE_NAME=${APP_NAME}_WEB2_NODE -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e JVM_ROUTE=route2 -e APP_NAME=$APP_NAME --link db:db --link ws:ws --link jms:jms --link controller:controller -d appdynamics/ecommerce-tomcat:$VERSION
+docker run --name web1 -h ${APP_NAME}-web1 -e web=true -e EVENT_ENDPOINT=${EVENT_ENDPOINT} -e NODE_NAME=${APP_NAME}_WEB2_NODE -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e JVM_ROUTE=route2 -e APP_NAME=$APP_NAME --link db:db --link ws:ws --link jms:jms --link controller:controller -d appdynamics/ecommerce-tomcat:$VERSION
 sleep 30
 
 docker run --name=lbr --link web:web --link web1:web1 -p 80:80 -d appdynamics/ecommerce-lbr:$VERSION
-docker run --name msg -e jms=true -e NODE_NAME=${APP_NAME}_JMS_NODE -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e APP_NAME=$APP_NAME --link db:db --link jms:jms --link oracle-db:oracle-db --link fulfillment:fulfillment --link controller:controller -d appdynamics/ecommerce-tomcat:$VERSION
+docker run --name msg -h ${APP_NAME}-msg -e jms=true -e EVENT_ENDPOINT=${EVENT_ENDPOINT} -e NODE_NAME=${APP_NAME}_JMS_NODE -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e APP_NAME=$APP_NAME --link db:db --link jms:jms --link oracle-db:oracle-db --link fulfillment:fulfillment --link controller:controller -d appdynamics/ecommerce-tomcat:$VERSION
 sleep 30
 
 docker run --name=load-gen --link lbr:lbr -d appdynamics/ecommerce-load:$VERSION
