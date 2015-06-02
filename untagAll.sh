04b414ccda96c0893579b72b7ae908b0012f17bd
Mark Prichard <mqprichard@gmail.com>
2015-06-02 13:46:45 -0700

    Added script to rmi all tagged images

---
+++ untagAll.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+if [ -z "$1" ]; then
+  read -e -p "Version: " TAG_VERSION;
+else
+        export TAG_VERSION=$1;
+fi
+
+export TOMCAT_LATEST=`docker images | grep 'tomcat' | grep 'latest' | awk '{print $3}'`
+export LBR_LATEST=`docker images | grep 'lbr' | grep 'latest' | awk '{print $3}'`
+export DBAGENT_LATEST=`docker images | grep 'dbagent' | grep 'latest' | awk '{print $3}'`
+export SYNAPSE_LATEST=`docker images | grep 'synapse' | grep 'latest' | awk '{print $3}'`
+export FULFILLMENT_CLIENT_LATEST=`docker images | grep 'fulfillment-client' | grep 'latest' | awk '{print $3}'`
+
+docker rmi appdynamics/ecommerce-tomcat:$TAG_VERSION
+docker rmi appdynamics/ecommerce-lbr:$TAG_VERSION
+docker rmi appdynamics/ecommerce-dbagent:$TAG_VERSION
+docker rmi appdynamics/ecommerce-synapse:$TAG_VERSION
+docker rmi appdynamics/ecommerce-fulfillment-client:$TAG_VERSION
+
+if [[ `docker images -q --filter "dangling=true"` ]]
+then
+  echo
+  echo "Deleting intermediate containers..."
+  docker images -q --filter "dangling=true" | xargs docker rmi;
+fi
