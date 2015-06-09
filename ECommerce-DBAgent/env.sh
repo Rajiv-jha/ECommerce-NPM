6a3545b263ba1b444fa5091d4fdff6fad046123e
Mark Prichard <mqprichard@gmail.com>
2015-06-09 14:14:31 -0700

    Added controller-info.xml for DBAgent

---
+++ ECommerce-DBAgent/env.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+export AGENT_HOME=/dbagent
--- ECommerce-DBAgent/startup.sh
+++ ECommerce-DBAgent/startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 CWD=${PWD}
 
+source env.sh
+
 if [ -z "${CONTROLLER}" ]; then
 	export CONTROLLER="controller";
 fi
@@ -9,14 +11,16 @@ if [ -z "${APPD_PORT}" ]; then
 	export APPD_PORT=8090;
 fi
 
+CONTROLLER_INFO_SETTINGS="s/CONTROLLERHOST/${CONTROLLER}/g;
+s/CONTROLLERPORT/${APPD_PORT}/g;
+s/ACCOUNTACCESSKEY/${ACCESS_KEY}/g"
+
+sed -e "${CONTROLLER_INFO_SETTINGS}" /controller-info.xml > /${AGENT_HOME}/conf/controller-info.xml
+
 JAVA_OPTS="-Duser.timezone=UTC -Dappdynamics.controller.hostName=${CONTROLLER} -Dappdynamics.controller.port=${APPD_PORT} -Dappdynamics.agent.accountAccessKey=${ACCESS_KEY}";
-
 JAVA_OPTS="${JAVA_OPTS} -Xmx512m -XX:MaxPermSize=128m -Duser.timezone=UTC";
-
 echo $JAVA_OPTS;
 
 cd ${CATALINA_HOME}/bin;
-
 java ${JAVA_OPTS} -Ddbagent.name=$(hostname)-dbagent -jar ${AGENT_HOME}/db-agent.jar 
-
 cd ${CWD}
