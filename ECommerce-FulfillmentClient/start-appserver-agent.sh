418c1c4197b3da3dc3e5116125218df81c34d0a2
Mark Prichard <mqprichard@gmail.com>
2015-06-10 11:03:17 -0700

    Added accountAccessKey to controller-info.xml for FulfillmentClient

--- ECommerce-FulfillmentClient/start-appserver-agent.sh
+++ ECommerce-FulfillmentClient/start-appserver-agent.sh
@@ -2,6 +2,16 @@
 
 source /env.sh
 
+CONTROLLER_INFO_SETTINGS="s/CONTROLLERHOST/${CONTROLLER}/g;
+s/CONTROLLERPORT/${APPD_PORT}/g;
+s/APP/${APP_NAME}/g;s/TIER/${TIER_NAME}/g;
+s/NODE/${NODE_NAME}/g;
+s/FOO/${SIM_HIERARCHY_1}/g;
+s/BAR/${SIM_HIERARCHY_2}/g;
+s/BAZ/${HOSTNAME}/g;
+s/ACCOUNTACCESSKEY/${ACCESS_KEY}/g"
+
+sed -e "${CONTROLLER_INFO_SETTINGS}" /controller-info.xml > /${CATALINA_HOME}/appagent/conf/controller-info.xml
 echo "Starting Client ...."
 echo APP_AGENT_JAVA_OPTS: ${APP_AGENT_JAVA_OPTS};
 echo JMX_OPTS: ${JMX_OPTS}
