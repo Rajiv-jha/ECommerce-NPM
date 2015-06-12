3dd64bced66b29993431f5f3364e91854d01dee3
aleftik <aleftik@appdynamics.com>
2015-06-12 14:08:39 -0700

    Fixes for Manual EUEM Injection to replace collector hostname

--- ECommerce-LBR/update-rum-key.sh
+++ ECommerce-LBR/update-rum-key.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sed -i "s/'xx-xxx-xx'/'${1}'/g" $HTTPD_24/conf.modules.d/02-appd.conf
 
+sed -i "s/'xx-xxx-xx'/'${1}'/g" $HTTPD_24/conf.modules.d/02-appd.conf
+sed -i "s/col.eum-appdynamics.com/${2}/g" ${HTTPD_DOC_ROOT}/adrum.js
+/etc/init.d/httpd24-httpd stop
+/etc/init.d/httpd24-httpd start
