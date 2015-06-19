c6e8c3c841c5d6067cf9b6ed29dad2d469757529
Val <vchibisov@appdynamics.com>
2015-06-19 12:11:06 -0700

    Adding missing replacers for cdn.appdynamics.com

--- ECommerce-Angular/update-rum-key.sh
+++ ECommerce-Angular/update-rum-key.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+sed -i "s/xx-xxx-xx/${1}/g" /tomcat/webapps/angular/index.html
 sed -i "s/APP_KEY_NOT_SET/${1}/g" adrum.js
 sed -i "s/col.eum-appdynamics.com/${2}/g" adrum.js
+sed -i "s/http:\/\/cdn.appdynamics.com/http:\/\/s3-us-west-1.amazonaws.com\/jsagent-trunk.appdynamics.com/g" adrum.js
 
 cp adrum.js /tomcat/webapps/angular/js/
