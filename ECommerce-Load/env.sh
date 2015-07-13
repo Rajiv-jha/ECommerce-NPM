44cfd9a1dae53b82b0d8a6192778e1daf2cbee70
swetharavichandran03 <swetha.ravichandran@appdynamics.com>
2015-07-13 10:43:21 -0700

    Added loadgen for angular

--- ECommerce-Load/env.sh
+++ ECommerce-Load/env.sh
@@ -26,7 +26,26 @@ if [ -z "${TARGET_PORT}" ]; then
 fi
 
 # 6th param to LoadRunner call
+if [ -z "${TARGET_ANGULARPORT}" ]; then
+        export TARGET_ANGULARPORT="8080";
+fi
+
+# 7th param to LoadRunner call
+if [ -z "${MYSQL_HOST}" ]; then
+        export MYSQL_HOST="db";
+fi
+
+# 8th param to LoadRunner call
+if [ -z "${MYSQL_USERNAME}" ]; then
+        export MYSQL_USERNAME="root";
+fi
+
+# 9th param to LoadRunner call
+if [ -z "${MYSQL_PWD}" ]; then
+        export MYSQL_PWD="singcontroller";
+fi
+
+# 10th param to LoadRunner call
 if [ -z "${WAIT_TIME}" ]; then
         export WAIT_TIME="1000";
 fi
-
