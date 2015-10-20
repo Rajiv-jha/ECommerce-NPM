b3325cae3217e1c5adb694aaa190e848369b094d
Mark Prichard <mqprichard@gmail.com>
2015-10-20 15:41:52 -0700

    Added DOCKER_REGISTRY environment variable

--- run.sh
+++ run.sh
@@ -32,6 +32,11 @@ else
 fi
 echo "Application Name: $APP_NAME"
 
+# Docker Registry - default to public dockerhub account
+if [ -z "${DOCKER_REGISTRY}" ]; then
+    export DOCKER_REGISTRY="appdynamics"
+fi
+
 checkOptional() {
     eval VALUE=\$$1
     if [ -z "$VALUE" ]; then
@@ -60,97 +65,100 @@ checkEnv() {
     checkRequired "ACCOUNT_NAME"
     checkRequired "ACCESS_KEY"
     checkRequired "EVENT_ENDPOINT"
+
+    # Docker Registry
+    checkRequired "DOCKER_REGISTRY"
 }
 
 checkEnv
 
-echo -n "oracle-db: "; docker run --name oracle-db -d -p 1521:1521 -p 2222:22 appdynamics/ecommerce-oracle
-echo -n "db: "; docker run --name db -p 3306:3306 -p 2223:22 -e MYSQL_ROOT_PASSWORD=singcontroller -d appdynamics/ecommerce-mysql
-echo -n "jms: "; docker run --name jms -d appdynamics/ecommerce-activemq
+echo -n "oracle-db: "; docker run --name oracle-db -d -p 1521:1521 -p 2222:22 ${DOCKER_REGISTRY}/ecommerce-oracle
+echo -n "db: "; docker run --name db -p 3306:3306 -p 2223:22 -e MYSQL_ROOT_PASSWORD=singcontroller -d ${DOCKER_REGISTRY}/ecommerce-mysql
+echo -n "jms: "; docker run --name jms -d ${DOCKER_REGISTRY}/ecommerce-activemq
 sleep 30
 
 echo -n "rds-dbwrapper: "; docker run --name rds-dbwrapper -h rds-dbwrapper \
 	-e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e EVENT_ENDPOINT=${EVENT_ENDPOINT} \
-  -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
-  -e NODE_NAME=${APP_NAME}_NODE -e APP_NAME=${APP_NAME} -e TIER_NAME=Address-Services \
-  -e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
-  --link oracle-db:oracle-db -d appdynamics/ecommerce-dbwrapper:$VERSION
+        -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
+        -e NODE_NAME=${APP_NAME}_NODE -e APP_NAME=${APP_NAME} -e TIER_NAME=Address-Services \
+        -e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
+        --link oracle-db:oracle-db -d ${DOCKER_REGISTRY}/ecommerce-dbwrapper:$VERSION
 
 echo -n "ws: "; docker run --name ws -h ${APP_NAME}-ws -e create_schema=true -e ws=true \
-	-e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e EVENT_ENDPOINT=${EVENT_ENDPOINT} \
-	-e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
-	-e NODE_NAME=${APP_NAME}_WS_NODE -e APP_NAME=$APP_NAME -e TIER_NAME=Inventory-Services \
-	-e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} --link db:db \
-	--link jms:jms --link oracle-db:oracle-db --link rds-dbwrapper:rds-dbwrapper -d appdynamics/ecommerce-tomcat:$VERSION
+        -e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e EVENT_ENDPOINT=${EVENT_ENDPOINT} \
+        -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
+        -e NODE_NAME=${APP_NAME}_WS_NODE -e APP_NAME=$APP_NAME -e TIER_NAME=Inventory-Services \
+        -e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} --link db:db \
+        --link jms:jms --link oracle-db:oracle-db --link rds-dbwrapper:rds-dbwrapper -d ${DOCKER_REGISTRY}/ecommerce-tomcat:$VERSION
 
 echo -n "web: "; docker run --name web -h ${APP_NAME}-web -e JVM_ROUTE=route1 -e web=true \
-	-e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e EVENT_ENDPOINT=${EVENT_ENDPOINT} \
-	-e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
-	-e NODE_NAME=${APP_NAME}_WEB1_NODE -e APP_NAME=$APP_NAME -e TIER_NAME=ECommerce-Services \
-	-e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
-	--link db:db --link oracle-db:oracle-db --link ws:ws --link jms:jms -d appdynamics/ecommerce-tomcat:$VERSION
+        -e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e EVENT_ENDPOINT=${EVENT_ENDPOINT} \
+        -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
+        -e NODE_NAME=${APP_NAME}_WEB1_NODE -e APP_NAME=$APP_NAME -e TIER_NAME=ECommerce-Services \
+        -e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
+        --link db:db --link oracle-db:oracle-db --link ws:ws --link jms:jms -d ${DOCKER_REGISTRY}/ecommerce-tomcat:$VERSION
 sleep 30
 
 echo -n "fulfillment: "; docker run --name fulfillment -h ${APP_NAME}-fulfillment -e web=true \
-	-e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e EVENT_ENDPOINT=${EVENT_ENDPOINT} \
-	-e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
-	-e NODE_NAME=Fulfillment -e APP_NAME=${APP_NAME}-Fulfillment -e TIER_NAME=Fulfillment-Services \
-	-e AWS_ACCESS_KEY=${AWS_ACCESS_KEY} -e AWS_SECRET_KEY=${AWS_SECRET_KEY} \
-	-e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
-	--link db:db --link ws:ws --link jms:jms --link oracle-db:oracle-db -d appdynamics/ecommerce-tomcat:$VERSION
+        -e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e EVENT_ENDPOINT=${EVENT_ENDPOINT} \
+        -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
+        -e NODE_NAME=Fulfillment -e APP_NAME=${APP_NAME}-Fulfillment -e TIER_NAME=Fulfillment-Services \
+        -e AWS_ACCESS_KEY=${AWS_ACCESS_KEY} -e AWS_SECRET_KEY=${AWS_SECRET_KEY} \
+        -e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
+        --link db:db --link ws:ws --link jms:jms --link oracle-db:oracle-db -d ${DOCKER_REGISTRY}/ecommerce-tomcat:$VERSION
 sleep 30
 
 echo -n "fulfillment-client: "; docker run --name fulfillment-client -h ${APP_NAME}-fulfillment-client \
-	-e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} \
+        -e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} \
         -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
-	-e NODE_NAME=FulfillmentClient1 -e APP_NAME=${APP_NAME}-Fulfillment -e TIER_NAME=Fulfillment-Client-Services \
-	-e AWS_ACCESS_KEY=${AWS_ACCESS_KEY} -e AWS_SECRET_KEY=${AWS_SECRET_KEY} \
-	-e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
-	-d appdynamics/ecommerce-fulfillment-client:$VERSION
+        -e NODE_NAME=FulfillmentClient1 -e APP_NAME=${APP_NAME}-Fulfillment -e TIER_NAME=Fulfillment-Client-Services \
+        -e AWS_ACCESS_KEY=${AWS_ACCESS_KEY} -e AWS_SECRET_KEY=${AWS_SECRET_KEY} \
+        -e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
+-d ${DOCKER_REGISTRY}/ecommerce-fulfillment-client:$VERSION
 sleep 30
 
 echo -n "web1: "; docker run --name web1 -h ${APP_NAME}-web1 -e JVM_ROUTE=route2 -e web=true \
-	-e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e EVENT_ENDPOINT=${EVENT_ENDPOINT} \
-	-e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
-	-e NODE_NAME=${APP_NAME}_WEB2_NODE -e APP_NAME=$APP_NAME -e TIER_NAME=ECommerce-Services \
-	-e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
-	--link db:db --link oracle-db:oracle-db --link ws:ws --link jms:jms -d appdynamics/ecommerce-tomcat:$VERSION
+        -e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e EVENT_ENDPOINT=${EVENT_ENDPOINT} \
+        -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
+        -e NODE_NAME=${APP_NAME}_WEB2_NODE -e APP_NAME=$APP_NAME -e TIER_NAME=ECommerce-Services \
+        -e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
+        --link db:db --link oracle-db:oracle-db --link ws:ws --link jms:jms -d ${DOCKER_REGISTRY}/ecommerce-tomcat:$VERSION
 sleep 30
 
 echo -n "customer-survey: "; docker run --name customer-survey -h ${APP_NAME}-customer-survey \
-	-e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} \
+        -e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} \
         -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
-	-e NODE_NAME=${APP_NAME}_Customer-Survey -e APP_NAME=${APP_NAME} -e TIER_NAME=Customer-Survey-Services \
-	-e AWS_ACCESS_KEY=${AWS_ACCESS_KEY} -e AWS_SECRET_KEY=${AWS_SECRET_KEY} \
-	-e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
-	-d --link jms:jms --link db:db --link oracle-db:oracle-db --link ws:ws appdynamics/ecommerce-survey-client:$VERSION
+        -e NODE_NAME=${APP_NAME}_Customer-Survey -e APP_NAME=${APP_NAME} -e TIER_NAME=Customer-Survey-Services \
+        -e AWS_ACCESS_KEY=${AWS_ACCESS_KEY} -e AWS_SECRET_KEY=${AWS_SECRET_KEY} \
+        -e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
+        -d --link jms:jms --link db:db --link oracle-db:oracle-db --link ws:ws ${DOCKER_REGISTRY}/ecommerce-survey-client:$VERSION
 sleep 30
 
 echo -n "lbr: "; docker run --name=lbr -h ${APP_NAME}-lbr \
-	-e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
-	-e APP_NAME=${APP_NAME} -e TIER_NAME=Web-Tier-Services -e NODE_NAME=${APP_NAME}-Apache \
-	-e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e EVENT_ENDPOINT=${EVENT_ENDPOINT} \
-	-e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
-	--link web:web --link web1:web1 -p 80:80 -d appdynamics/ecommerce-lbr:$VERSION
+        -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
+        -e APP_NAME=${APP_NAME} -e TIER_NAME=Web-Tier-Services -e NODE_NAME=${APP_NAME}-Apache \
+        -e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e EVENT_ENDPOINT=${EVENT_ENDPOINT} \
+        -e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
+        --link web:web --link web1:web1 -p 80:80 -d ${DOCKER_REGISTRY}/ecommerce-lbr:$VERSION
 
 echo -n "msg: "; docker run --name msg -h ${APP_NAME}-msg -e jms=true \
-	-e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e EVENT_ENDPOINT=${EVENT_ENDPOINT} \
-	-e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
-	-e NODE_NAME=${APP_NAME}_JMS_NODE -e APP_NAME=$APP_NAME -e TIER_NAME=Order-Processing-Services \
-	-e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
-	--link db:db --link jms:jms --link oracle-db:oracle-db --link fulfillment:fulfillment -d appdynamics/ecommerce-tomcat:$VERSION
+        -e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e EVENT_ENDPOINT=${EVENT_ENDPOINT} \
+        -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
+        -e NODE_NAME=${APP_NAME}_JMS_NODE -e APP_NAME=$APP_NAME -e TIER_NAME=Order-Processing-Services \
+        -e SIM_HIERARCHY_1=${SIM_HIERARCHY_1} -e SIM_HIERARCHY_2=${SIM_HIERARCHY_2} \
+        --link db:db --link jms:jms --link oracle-db:oracle-db --link fulfillment:fulfillment -d ${DOCKER_REGISTRY}/ecommerce-tomcat:$VERSION
 sleep 30
 echo -n "angular: "; docker run --name angular -h ${APP_NAME}-angular \
-	--link lbr:lbr -p 8080:8080 -d appdynamics/ecommerce-angular:$VERSION
+        --link lbr:lbr -p 8080:8080 -d ${DOCKER_REGISTRY}/ecommerce-angular:$VERSION
 
 echo -n "faultinjection: "; docker run --name faultinjection -h ${APP_NAME}-faultinjection \
-  	--link lbr:lbr -p 8088:8080 -d appdynamics/ecommerce-faultinjection:$VERSION
+        --link lbr:lbr -p 8088:8080 -d ${DOCKER_REGISTRY}/ecommerce-faultinjection:$VERSION
 
-#Waiting for all services to be running before being accessed by load-gen
+# Wait for all services to be running before starting load-gen
 sleep 30
 
-echo -n "load-gen: "; docker run --name=load-gen --link lbr:lbr --link angular:angular -d appdynamics/ecommerce-load:$VERSION
+echo -n "load-gen: "; docker run --name=load-gen --link lbr:lbr --link angular:angular -d ${DOCKER_REGISTRY}/ecommerce-load:$VERSION
 
 echo -n "dbagent: "; docker run --name dbagent \
         -e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e ACCESS_KEY=${ACCESS_KEY} \
-        --link db:db --link oracle-db:oracle-db -d appdynamics/ecommerce-dbagent:$VERSION
+        --link db:db --link oracle-db:oracle-db -d ${DOCKER_REGISTRY}/ecommerce-dbagent:$VERSION
