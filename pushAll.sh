adedf94d5fadb3550062746189abb4e32d3bbf0a
Mark Prichard <mqprichard@gmail.com>
2015-10-21 18:19:12 -0700

    Support pushing to multiple registries

--- pushAll.sh
+++ pushAll.sh
@@ -1,16 +1,22 @@
 #! /bin/bash
 
-if [ "$#" -ne 3 ]; then
-  read -e -p "Application: " APP_NAME;
-  read -e -p "Version: " TAG_VERSION;
-  read -e -p "Registry: " REGISTRY;
-else
-  export APP_NAME=$1;
-  export TAG_VERSION=$2;
-  export REGISTRY=$3;
-fi
-
-for i in $(docker images | grep ${REGISTRY} | grep ${APP_NAME} | grep $TAG_VERSION | awk {'print $1'}); do
+if [ "$#" -eq 1 ]; then
+  export TAG_VERSION=$1
+  echo "Pushing images with tag: ${TAG_VERSION} to dockerhub"
+  for i in $(docker images | grep "appdynamics/ecommerce" | grep $TAG_VERSION | awk {'print $1'}); do
     echo Pushing:  $i:${TAG_VERSION}
     docker push $i:$TAG_VERSION
-done
+  done
+elif [ "$#" -eq 3 ]; then
+  export TAG_VERSION=$1;
+  export REGISTRY=$2;
+  export APP_NAME=$3;
+  echo "Pushing images with tag: ${TAG_VERSION} to ${REGISTRY}/${APP_NAME}"
+  for i in $(docker images | grep ${REGISTRY} | grep ${APP_NAME} | grep $TAG_VERSION | awk {'print $1'}); do
+    echo Pushing:  $i:${TAG_VERSION}
+    docker push $i:$TAG_VERSION
+  done
+else
+  echo "Usage: pushAll.sh <tag> [<registry> <application>]"
+  exit
+fi
