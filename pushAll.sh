41b80240a30549dc416b744b27b15eb10de67a53
Jeff Morgan <jeff.morgan@appdynamics.com>
2016-04-04 19:44:18 +0000

    fixed Docker file and add -f to pushAll.sh

--- pushAll.sh
+++ pushAll.sh
@@ -5,7 +5,7 @@ if [ "$#" -eq 1 ]; then
   echo "Pushing images with tag: ${TAG_VERSION} to dockerhub"
   for i in $(docker images | grep "appdynamics/ecommerce" | grep $TAG_VERSION | awk {'print $1'}); do
     echo Pushing:  $i:${TAG_VERSION}
-    docker push $i:$TAG_VERSION
+    docker push -f $i:$TAG_VERSION
   done
 elif [ "$#" -eq 2 ]; then
   export TAG_VERSION=$1;
@@ -13,7 +13,7 @@ elif [ "$#" -eq 2 ]; then
   echo "Pushing images with tag: ${TAG_VERSION} to ${REGISTRY}"
   for i in $(docker images | grep ${REGISTRY} | grep $TAG_VERSION | awk {'print $1'}); do
     echo Pushing:  $i:${TAG_VERSION}
-    docker push $i:$TAG_VERSION
+    docker push -f $i:$TAG_VERSION
   done
 else
   echo "Usage: pushAll.sh <tag> [<registry>]"
